c603740f47be962be8145c30564e295cff1224c2
Andrei Monea <amonea@deloittece.com>
2022-04-18 23:12:00 +0300

    Fixing frontend

--- src/WebUI/client-app/src/components/RegistrationForm/RegistrationForm.js
+++ src/WebUI/client-app/src/components/RegistrationForm/RegistrationForm.js
@@ -1,8 +1,14 @@
 import React from 'react'
 import GetListed from '../../pages/GetListed'
-import {FormWrapper,HeadingContainer,Title,Paragraph,Form,Label} from './RegistrationFormStyleElements'
-import {Input} from '../../elements/InputElement'
+import {FormWrapper,HeadingContainer,Title,Paragraph,Form,Label,Plus,UploadImg} from './RegistrationFormStyleElements'
+import {Input,FileInput,DateInput,RadioInput,TextArea} from '../../elements/InputElement'
+import addImg from '../../photos/plus.png'
 function RegistrationForm() {
+
+  const hiddenFileInput = React.useRef(null);
+  const handleClick = event => {
+    hiddenFileInput.current.click();
+  };
   return (
     <FormWrapper>
         <HeadingContainer>
@@ -14,19 +20,26 @@ function RegistrationForm() {
             </Paragraph>
         </HeadingContainer>
         <Form>
-            
+            <Label>Logo</Label>
+            <FileInput  onClick={handleClick}>
+            <Plus src={addImg}/>
+            <UploadImg>Update Image</UploadImg>
+                <input type="file"
+              ref={hiddenFileInput}
+              style={{display:'none'}} />
+            </FileInput>
             <Label>Name</Label>
             <Input/>
             <Label>Symbol</Label>
             <Input/>
             <Label>Description</Label>
-            <Input width='740px' height='130px'/>
+            <TextArea width='60%' height='130px'/>
             <Label>Contract Address</Label>
             <Input width='400px'/>
             <Label>Launch Date</Label>
-            <Input width='400px'/>
+            <DateInput type={'date'} width='250px'/>
             <Label>Presale Phase?</Label>
-            <Input width='400px'/>
+            <RadioInput type={'radio'} width='400px'/>
             <Label>Website Link</Label>
             <Input width='400px'/>
             <Label>Twitter Link</Label>
@@ -35,8 +48,6 @@ function RegistrationForm() {
             <Input width='400px'/>
             <Label>Discord Link</Label>
             <Input width='400px'/>
-
-            
         </Form>
 
 
