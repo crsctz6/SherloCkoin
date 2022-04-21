1fc2a18245ae22eeaf46c7820501c9bbf0438a86
Andrei Monea <amonea@deloittece.com>
2022-04-21 18:32:55 +0300

    Title and buttons change

--- src/WebUI/client-app/src/elements/ButtonElement.js
+++ src/WebUI/client-app/src/elements/ButtonElement.js
@@ -17,5 +17,11 @@ export const Button = styled.button`
   width: ${(props) => props.width ?? "auto"};
   height: ${(props) => props.height ?? "auto"};
   cursor: pointer;
+  transition: all 1000ms cubic-bezier(0.075, 0.82, 0.165, 1) 0s;
+  cursor: pointer;
+  :hover {
+  transform: scale(1.03);
+  background:${Backgrounds.buttonHover};
+}
 
 `;
